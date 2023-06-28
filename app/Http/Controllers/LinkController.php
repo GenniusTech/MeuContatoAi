b1dc850ff7b4455ffbccbf42bf827e7d1fad098d
wendel <uendel.1@hotmail.com>
2023-06-28 05:12:23 -0300

    link

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -8,14 +8,26 @@ use Illuminate\Http\Request;
 class LinkController extends Controller
 {
     public function handle($any)
-{
-    $link = Link::where('rota', $any)->first();
+    {
+        $link = Link::where('rota', $any)->first();
 
-    if ($link) {
-        return redirect()->to($link->url);
+        if ($link) {
+            $connections = $link->connections()->orderBy('lead')->take(20)->get();
+
+            if ($connections->isNotEmpty()) {
+                $connection = $connections->first();
+
+                // Incrementa o valor de lead em 1
+                $connection->lead += 1;
+                $connection->save();
+
+                // Redireciona para a URL do connection
+                return redirect($connection->url);
+            }
+        }
+
+        // Redireciona para a rota "index" (ou qualquer outra rota padrão que você definiu)
+        return redirect()->route('index');
     }
-
-    return redirect()->route('index');
-}
 
 }
